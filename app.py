8bb4c42a41fb816faf83f2260d5ce7b05a6795f7
danielgur <danielgur2105@gmail.com>
2012-10-11 00:44:48 -0400

    adding get functionality

--- app.py
+++ app.py
@@ -5,9 +5,53 @@ import simplejson
 import os
 from functools import wraps
 from bs4 import BeautifulSoup
-from flask import Flask, request, current_app
+from flask import Flask, request, current_app, make_response
+from functools import update_wrapper
+from datetime import timedelta
+
 
 app = Flask(__name__)
+
+def crossdomain(origin=None, methods=None, headers=None,
+                max_age=21600, attach_to_all=True,
+                automatic_options=True):
+    if methods is not None:
+        methods = ', '.join(sorted(x.upper() for x in methods))
+    if headers is not None and not isinstance(headers, basestring):
+        headers = ', '.join(x.upper() for x in headers)
+    if not isinstance(origin, basestring):
+        origin = ', '.join(origin)
+    if isinstance(max_age, timedelta):
+        max_age = max_age.total_seconds()
+
+    def get_methods():
+        if methods is not None:
+            return methods
+
+        options_resp = current_app.make_default_options_response()
+        return options_resp.headers['allow']
+
+    def decorator(f):
+        def wrapped_function(*args, **kwargs):
+            if automatic_options and request.method == 'OPTIONS':
+                resp = current_app.make_default_options_response()
+            else:
+                resp = make_response(f(*args, **kwargs))
+            if not attach_to_all and request.method != 'OPTIONS':
+                return resp
+
+            h = resp.headers
+
+            h['Access-Control-Allow-Origin'] = origin
+            h['Access-Control-Allow-Methods'] = get_methods()
+            h['Access-Control-Max-Age'] = str(max_age)
+            if headers is not None:
+                h['Access-Control-Allow-Headers'] = headers
+            return resp
+
+        f.provide_automatic_options = False
+        return update_wrapper(wrapped_function, f)
+    return decorator
 
 def support_jsonp(f):
     """Wraps JSONified output for JSONP"""
@@ -23,6 +67,7 @@ def support_jsonp(f):
 
 @app.route('/',methods=['POST', 'GET'])
 @support_jsonp
+@crossdomain(origin='*')
 def start():
     username = None
     password = None
